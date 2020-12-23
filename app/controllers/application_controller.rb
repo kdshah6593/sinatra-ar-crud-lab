7815ae3515ca301dd8a55e52ef473b62d22920a6
Kunal <kdshah6593@gmail.com>
2020-12-23 14:24:20 -0500

    commit tree d938ae2234614ff792c3eccd30157d5ffe042573

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,40 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  patch '/articles/:id' do
+    @article2 = Article.find(params[:id])
+    @article2.update(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article2.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article_edit = Article.find(params[:id])
+    erb :edit
+  end
+
+  delete '/articles/:id' do
+    @article_delete = Article.find(params[:id])
+    @article_delete.destroy
+    redirect "/articles"
+  end
 end
